1e91214cf8d0caa960e303653e0814e415126963
Tidominer <75866836+Tidominer@users.noreply.github.com>
2025-08-27 04:40:01 +0330

    Added ignoreCondition to Raycast and CircleCast methods

--- SimPhys/PhysicsCaster.cs
+++ SimPhys/PhysicsCaster.cs
@@ -6,24 +6,34 @@ namespace SimPhys
 {
     public class PhysicsCaster
     {
-        private readonly List<Entity> entities;
+        private readonly List<Entity> _entities;
 
         public PhysicsCaster(List<Entity> entityList)
         {
-            this.entities = entityList;
+            this._entities = entityList;
         }
 
         /// <summary>
         /// Casts a ray against all entities in the simulation space.
         /// </summary>
-        public bool Raycast(Vector2 origin, Vector2 direction, decimal maxDistance, out RaycastHit hitInfo)
+        /// <param name="origin">The starting point of the ray in world space.</param>
+        /// <param name="direction">The normalized direction of the ray.</param>
+        /// <param name="maxDistance">The maximum distance the ray should travel.</param>
+        /// <param name="hitInfo">If a hit occurs, this contains detailed information about the closest hit.</param>
+        /// <param name="ignoreCondition">A function to filter which entities should be ignored by the cast. Return true to ignore.</param>
+        public bool Raycast(Vector2 origin, Vector2 direction, decimal maxDistance, out RaycastHit hitInfo, Func<Entity,bool> ignoreCondition = null)
         {
             hitInfo = null;
             var closestHit = new RaycastHit { Distance = maxDistance };
             bool hasHit = false;
 
-            foreach (var entity in entities)
+            foreach (var entity in _entities)
             {
+                if (ignoreCondition != null && ignoreCondition(entity))
+                {
+                    continue;
+                }
+                
                 if (RaycastVsEntity(origin, direction, entity, out var currentHit) && currentHit.Distance < closestHit.Distance)
                 {
                     closestHit = currentHit;
@@ -42,14 +52,25 @@ namespace SimPhys
         /// <summary>
         /// Sweeps a circle through the simulation space and checks for intersections.
         /// </summary>
-        public bool CircleCast(Vector2 origin, decimal radius, Vector2 direction, decimal maxDistance, out CircleCastHit hitInfo)
+        /// <param name="origin">The starting center position of the circle in world space.</param>
+        /// <param name="radius">The radius of the circle to sweep.</param>
+        /// <param name="direction">The normalized direction of the sweep.</param>
+        /// <param name="maxDistance">The maximum distance the circle should travel.</param>
+        /// <param name="hitInfo">If a hit occurs, this contains detailed information about the closest hit.</param>
+        /// <param name="ignoreCondition">A function to filter which entities should be ignored by the cast. Return true to ignore.</param>
+        public bool CircleCast(Vector2 origin, decimal radius, Vector2 direction, decimal maxDistance, out CircleCastHit hitInfo, Func<Entity, bool> ignoreCondition = null)
         {
             hitInfo = null;
             var closestHit = new CircleCastHit { Distance = maxDistance };
             bool hasHit = false;
 
-            foreach (var entity in entities)
+            foreach (var entity in _entities)
             {
+                if (ignoreCondition != null && ignoreCondition(entity))
+                {
+                    continue;
+                }
+                
                 if (CircleCastVsEntity(origin, radius, direction, entity, out var currentHit) && currentHit.Distance < closestHit.Distance)
                 {
                     closestHit = currentHit;
--- SimPhys/SimulationSpace.cs
+++ SimPhys/SimulationSpace.cs
@@ -15,7 +15,6 @@ namespace SimPhys
         {
             SpaceSettings = settings;
             _caster = new PhysicsCaster(this.Entities);
-            Entities = new List<Entity>();
         }
 
         /// <summary>
@@ -88,17 +87,28 @@ namespace SimPhys
         /// <summary>
         /// Casts a ray against all entities in the simulation space.
         /// </summary>
-        public bool Raycast(Vector2 origin, Vector2 direction, decimal maxDistance, out RaycastHit hitInfo)
+        /// <param name="origin">The starting point of the ray in world space.</param>
+        /// <param name="direction">The normalized direction of the ray.</param>
+        /// <param name="maxDistance">The maximum distance the ray should travel.</param>
+        /// <param name="hitInfo">If a hit occurs, this contains detailed information about the closest hit.</param>
+        /// <param name="ignoreCondition">A function to filter which entities should be ignored by the cast. Return true to ignore.</param>
+        public bool Raycast(Vector2 origin, Vector2 direction, decimal maxDistance, out RaycastHit hitInfo, Func<Entity, bool> ignoreCondition = null)
         {
-            return _caster.Raycast(origin, direction, maxDistance, out hitInfo);
+            return _caster.Raycast(origin, direction, maxDistance, out hitInfo, ignoreCondition);
         }
 
         /// <summary>
         /// Sweeps a circle through the simulation space and checks for intersections.
         /// </summary>
-        public bool CircleCast(Vector2 origin, decimal radius, Vector2 direction, decimal maxDistance, out CircleCastHit hitInfo)
+        /// <param name="origin">The starting center position of the circle in world space.</param>
+        /// <param name="radius">The radius of the circle to sweep.</param>
+        /// <param name="direction">The normalized direction of the sweep.</param>
+        /// <param name="maxDistance">The maximum distance the circle should travel.</param>
+        /// <param name="hitInfo">If a hit occurs, this contains detailed information about the closest hit.</param>
+        /// <param name="ignoreCondition">A function to filter which entities should be ignored by the cast. Return true to ignore.</param>
+        public bool CircleCast(Vector2 origin, decimal radius, Vector2 direction, decimal maxDistance, out CircleCastHit hitInfo, Func<Entity, bool> ignoreCondition = null)
         {
-            return _caster.CircleCast(origin, radius, direction, maxDistance, out hitInfo);
+            return _caster.CircleCast(origin, radius, direction, maxDistance, out hitInfo, ignoreCondition);
         }
 
         #endregion
